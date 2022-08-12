773bd632c1cae1bdb7159220b61dd3726bc2d495
DOSEOKYEONG <dho88dho@naver.com>
2022-08-13 01:09:42 +0900

    add signup

--- src/main/java/com/ll/exam/sbb/user/UserController.java
+++ src/main/java/com/ll/exam/sbb/user/UserController.java
@@ -3,6 +3,7 @@ package com.ll.exam.sbb.user;
 import com.ll.exam.sbb.question.QuestionForm;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,7 +35,19 @@ public class UserController {
             return "signup_form";
         }
 
-        SiteUser user = userService.create(userCreateForm.getUsername(), userCreateForm.getPassword1(), userCreateForm.getEmail());
+        try {
+            SiteUser user = userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getPassword1(), userCreateForm.getEmail());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
 
         return "redirect:/";
     }
